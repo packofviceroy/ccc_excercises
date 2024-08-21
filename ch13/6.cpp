7085ffa14eac48630c4fd2c1c94152b48a7ddabf
dumboProMac <hekutok@gmail.com>
2024-08-21 14:07:07 +0300

    13.6 complete

--- ch13/6.cpp
+++ ch13/6.cpp
@@ -1,20 +1,44 @@
 #include <iostream>
 #include <vector>
 
-template <typename T> 
-struct Matrix{
-    Matrix(size_t col, size_t row){
-        for( int i = 0; i < col; i++)
-            columns.push_back(std::vector<T>(row));
+template <typename T>
+struct Matrix
+{
+    Matrix() = delete;
+    Matrix(size_t row, size_t col) : rows_len(row), col_len(col)
+    {
+        for (int i = 0; i < row; i++)
+            data.push_back(std::vector<T>(col));
     }
-    
+
+    T &at(size_t row, size_t col)
+    {
+        if (row >= rows_len || col >= col_len)
+            throw std::out_of_range{"Index invalid."};
+        return data[row][col];
+    }
+
+    void printMatrix(){
+        for (auto vec: data){
+            for (auto el:vec)
+            {
+                std::cout << el << ',';
+            }
+            std::cout << std::endl;
+        }
+    }
+
 private:
-    std::vector<std::vector<T>> columns;
-    std::vector<T> rows;
+    size_t col_len;
+    size_t rows_len;
+    std::vector<std::vector<T>> data;
 };
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-
+    Matrix<int> *m = new Matrix<int>(3, 3);
+    m->printMatrix();
+    m->at(0, 0) = 2;
+    m->printMatrix();
     return 0;
 }
