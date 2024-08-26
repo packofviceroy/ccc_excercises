e83b3df84121d87beb60bb2f838942ab6a2bb703
nikita <hekutok@gmail.com>
2024-08-26 16:57:17 +0300

    some from air

---
+++ ch6/Accounts.h
@@ -0,0 +1,8 @@
+//
+// Created by Nikita Shuplyakov on 20/06/2023.
+//
+
+#ifndef CH6_ACCOUNT_H
+#define CH6_ACCOUNT_H
+
+#endif //CH6_ACCOUNT_H
---
+++ ch6/Bank.h
@@ -0,0 +1,8 @@
+//
+// Created by Nikita Shuplyakov on 20/06/2023.
+//
+
+#ifndef CH6_BANK_H
+#define CH6_BANK_H
+
+#endif //CH6_BANK_H
